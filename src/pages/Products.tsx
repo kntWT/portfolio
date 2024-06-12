f32969d26c25c227423dbe4b46c720afa11c57db
kntWT <kento.wata0628@gmail.com>
2024-06-12 17:14:35 +0900

    feat: productページ実装

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,6 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { productService } from '@/configs/products';
+import useArticleCards from '@/components/common/ArticleCard';
+import { Article } from '@/@types/common';
+
+
 const Products = () => {
+    const [products, setProducts] = useState<Article[]>([]);
+    const ArticleCards = useArticleCards(products);
+
+    useEffect(() => {
+        const loadData = async() => {
+            const prod = await productService.getArticles();
+            setProducts(prod);
+        }
+        loadData();
+    }, []);
+
     return (
-        <></>
+        ArticleCards
     );
 }
 
