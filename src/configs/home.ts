963e97338760695e1f5368ed3108b41edbb96d3e
kntWT <kento.wata0628@gmail.com>
2026-04-05 16:35:34 +0900

    refactor: configデータをコンポーネント外に

---
+++ src/configs/home.ts
@@ -0,0 +1,81 @@
+const profile: string =
+  "明治大学大学院先端数理科学研究科の博士前期課程に在籍し，HCI（ヒューマンコンピュータインタラクション）の分野を研究\n\
+主に初心者の運転支援や，手書き文字練習の支援，スマートフォン利用時の姿勢矯正に関する研究に着手\n\
+また，ソフトウェアエンジニアとしてプログラミングスキルを活かしシステム開発にも着手\n\
+主にwebアプリケーションの開発に携わり，フロントエンド，バックエンド，インフラなど幅広く経験\n\
+";
+const hobbies: string[] = [
+  "ドラム",
+  "サッカー",
+  "卓球",
+  "スポーツ観戦",
+  "邦ロック",
+  "J-POP",
+  "謎解き",
+  "ルービックキューブ",
+];
+const career: { year: string; content: string }[] = [
+  { year: "2020/03", content: "明治大学付属明治高等学校卒業" },
+  {
+    year: "2020/04",
+    content: "明治大学総合数理学部先端メディアサイエンス学科入学",
+  },
+  {
+    year: "2024/03",
+    content: "明治大学総合数理学部先端メディアサイエンス学科卒業",
+  },
+  {
+    year: "2024/04",
+    content: "明治大学大学院先端数理科学研究科先端メディアサイエンス専攻入学",
+  },
+];
+
+const skills = [
+  "git",
+  "github",
+  "vscode",
+  "linux",
+  "bash",
+  "nginx",
+  "mysql",
+  "postgresql",
+  "unity",
+  "cs",
+  "java",
+  "maven",
+  "processing",
+  "arduino",
+  "php",
+  "html",
+  "css",
+  "js",
+  "ts",
+  "npm",
+  "pnpm",
+  "nodejs",
+  "jest",
+  "vite",
+  "react",
+  "nextjs",
+  "vue",
+  "nuxtjs",
+  "pinia",
+  "svelte",
+  "angular",
+  "p5js",
+  "vuetify",
+  "materialui",
+  "sass",
+  "python",
+  "anaconda",
+  "tensorflow",
+  "django",
+  "fastapi",
+  "opencv",
+  "sklearn",
+  "go",
+  "docker",
+  "firebase",
+];
+
+export { career, hobbies, profile, skills };
