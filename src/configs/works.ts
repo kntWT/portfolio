6c5d19c521ff7c34ff1cfb2b5770c80a26ed1e01
kntWT <kento.wata0628@gmail.com>
2026-04-05 16:25:00 +0900

    feat: 英語の研究業績に対応

--- src/configs/works.ts
+++ src/configs/works.ts
@@ -1,81 +1,122 @@
-import { Article, Service, FetchedWork, DB } from "@/@types/article";
+import { Article, FetchedWork, Service } from "@/@types/article";
 
 // const loadDB = async (): Promise<DB> => {
 //     return await import("../../db.json") as DB;
 // }
 
 const loadWorks = async (): Promise<Article[]> => {
-    const param = import.meta.env.VITE_FETCH_WORKS_PARAMS.replace(/#/g, "$");
-    const url = `${import.meta.env.VITE_FETCH_WORKS_URL}?${param}`
+  const paramJa = import.meta.env.VITE_FETCH_WORKS_PARAMS_JA.replace(/#/g, "$");
+  const paramEn = import.meta.env.VITE_FETCH_WORKS_PARAMS_EN.replace(/#/g, "$");
+  const urlJa = `${import.meta.env.VITE_FETCH_WORKS_URL}?${paramJa}`;
+  const urlEn = `${import.meta.env.VITE_FETCH_WORKS_URL}?${paramEn}`;
 
-    const data = await fetch(url, {
-        method: 'GET',
-        headers: {
-            // 'Content-Type': 'application/json',
-            // "Access-Control-Allow-Origin": "*",
-        },
-        mode: 'cors',
+  const [dataJa, dataEn] = await Promise.all([
+    fetch(urlJa, {
+      method: "GET",
+      headers: {
+        // 'Content-Type': 'application/json',
+        // "Access-Control-Allow-Origin": "*",
+      },
+      mode: "cors",
     })
-        .then((res) => res.json())
-        .then((data) => data as FetchedWork[])
-        .catch((err) => console.error(err))
-    if (!data) return [];
+      .then((res) => res.json())
+      .then((data) => data as FetchedWork[])
+      .catch((err) => {
+        console.error(err);
+        return [];
+      }),
+    fetch(urlEn, {
+      method: "GET",
+      headers: {
+        // 'Content-Type': 'application/json',
+        // "Access-Control-Allow-Origin": "*",
+      },
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((data) => data as FetchedWork[])
+      .catch((err) => {
+        console.error(err);
+        return [];
+      }),
+  ]);
 
-    return data.map((d: FetchedWork) => {
-        const year = d.publication.publishedDate.match(/\d{4}/)?.[0] ?? "";
-        const externalUrl = Object.entries(d.embed).map(([key, value]) => ({
-            type: key === "youtube" ? "video" : "blog",
-            url: value,
-            text: key,
-        }));
-        const tags = [year, ...d.core.keywords,]
-        return {
-            id: parseInt(d.core.id),
-            title: d.core.title,
-            year: year,
-            links: [...externalUrl, { type: "url", url: `https://dl.nkmr-lab.org/papers/${d.core.id}`, text: "論文リポジトリ" }],
-            description: d.core.abstract,
-            image: d.core.thumbnail,
-            tags: tags,
-            cite: `${d.core.authors.join(", ")}.${d.core.title},${d.publication.bookTitle}, ${d.publication.volume}, ${d.publication.number}, oo.${d.publication.page}, ${d.publication.volume}, ${year}.`,
-        } as Article;
-    });
-}
+  const articlesJa = dataJa.map((d: FetchedWork) => {
+    return parseArticle(d);
+  });
+  const articlesEn = dataEn.map((d: FetchedWork) => {
+    return parseArticle(d);
+  });
+  return [...articlesJa, ...articlesEn].sort((a, b) => {
+    return a.date.getTime() - b.date.getTime();
+  });
+};
+
+const parseArticle = (d: FetchedWork) => {
+  const date = new Date(d.publication.publishedDate);
+  const year = date.getFullYear();
+  const externalUrl = Object.entries(d.embed).map(([key, value]) => ({
+    type: key === "youtube" ? "video" : "blog",
+    url: value,
+    text: key,
+  }));
+  const tags = [year, ...d.core.keywords];
+  return {
+    id: parseInt(d.core.id)!,
+    title: d.core.title,
+    year: year.toString(),
+    date: date,
+    links: [
+      ...externalUrl,
+      {
+        type: "url",
+        url: `https://dl.nkmr-lab.org/papers/${d.core.id}`,
+        text: "論文リポジトリ",
+      },
+    ],
+    description: d.core.abstract,
+    image: d.core.thumbnail ?? "noimage.png",
+    tags: tags,
+    cite: `${d.core.authors.join(", ")}.${d.core.title},${d.publication.bookTitle}, ${d.publication.volume}, ${d.publication.number}, oo.${d.publication.page}, ${d.publication.volume}, ${year}.`,
+  } as Article;
+};
 // const loadWorks = async (): Promise<Work[]> => {
 //     return (await loadDB()).works;
 // }
 
 const workService: Service = {
+  contents: [],
 
-    contents: [],
+  getNextId: () => {
+    return (
+      workService.contents.reduce(
+        (max, work) => Math.max(max, work?.id ?? 0),
+        0,
+      ) + 1
+    );
+  },
 
-    getNextId: () => {
-        return workService.contents.reduce((max, work) => Math.max(max, work?.id ?? 0), 0) + 1;
-    },
+  reload: async () => {
+    workService.contents = await loadWorks();
+  },
 
-    reload: async () => {
-        workService.contents = await loadWorks();
-    },
+  getArticles: async (): Promise<Article[]> => {
+    return new Promise((resolve, reject) => {
+      resolve(workService.contents);
+    });
+  },
 
-    getArticles: async (): Promise<Article[]> => {
-        return new Promise((resolve, reject) => {
-            resolve(workService.contents);
-        });
-    },
+  getArticleById: async (id: number): Promise<Article | null> => {
+    return new Promise((resolve, reject) => {
+      resolve(workService.contents.find((work) => work.id === id) ?? null);
+    });
+  },
 
-    getArticleById: async (id: number): Promise<Article | null> => {
-        return new Promise((resolve, reject) => {
-            resolve(workService.contents.find((work) => work.id === id) ?? null);
-        });
-    },
+  getArticlesByTag: async (tag: string): Promise<Article[]> => {
+    return new Promise((resolve, reject) => {
+      resolve(workService.contents.filter((work) => work.tags.includes(tag)));
+    });
+  },
+};
 
-    getArticlesByTag: async (tag: string): Promise<Article[]> => {
-        return new Promise((resolve, reject) => {
-            resolve(workService.contents.filter((work) => work.tags.includes(tag)));
-        });
-    },
-}
-
-export {
-    workService,
-}
+export { workService };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_PASSWORD: string;
-    readonly VITE_TWITTER_URL: string;
-    readonly VITE_GITHUB_URL: string;
-    readonly VITE_FETCH_WORKS_URL: string;
-    readonly VITE_FETCH_WORKS_PARAMS: string;
+  readonly VITE_PASSWORD: string;
+  readonly VITE_LINKEDIN_URL: string;
+  readonly VITE_GITHUB_URL: string;
+  readonly VITE_FETCH_WORKS_URL: string;
+  readonly VITE_FETCH_WORKS_PARAMS: string;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
