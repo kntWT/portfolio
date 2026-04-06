1bc80b080cff7009523598d13aeef36ac744065d
kntWT <kento.wata0628@gmail.com>
2026-04-06 12:28:50 +0900

    style: 不要なcss削除とフォントをLINE Seed JPを使うように

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
-import Header from "./components/common/header/Header";
 import Footer from "./components/common/footer/Footer";
+import Header from "./components/common/header/Header";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"LINE Seed JP", sans-serif',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <CssBaseline />
-        <Header />
-        <Router />
-        <Footer />
-      </BrowserRouter>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <BrowserRouter>
+          <CssBaseline />
+          <Header />
+          <Router />
+          <Footer />
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
